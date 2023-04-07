1148a5219a567c2e79c6d26c030bdd75a6b2e89e
Caio Mocny <91752440+caiomocny@users.noreply.github.com>
2023-04-07 18:15:21 -0300

    Update Automação de Sistemas e Processos.py

--- Automação de Sistemas e Processos.py
+++ Automação de Sistemas e Processos.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Automação de Sistemas e Processos com Python
-
-# ### Desafio:
-# Todos os dias, o nosso sistema atualiza as vendas do dia anterior.
-# O seu trabalho diário, como analista, é enviar um e-mail para a diretoria, assim que começar a trabalhar, com o faturamento e a quantidade de produtos vendidos no dia anterior
-# E-mail da diretoria: seugmail+diretoria@gmail.com<br>
-# Local onde o sistema disponibiliza as vendas do dia anterior: https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing
-# Para resolver isso, vamos usar o pyautogui, uma biblioteca de automação de comandos do mouse e do teclado
-
-#Instalar as bibliotecas
-# !pip install pyautogui
-# !pip install pyperclip
-
 #Passo 0: Importar as bibliotecas
 import pyautogui
 import pyperclip
